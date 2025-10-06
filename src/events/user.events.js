2030672e425ab908fda9d262943c0a2f1001557f
mortezapouramini <mortezapouramini2323@example.com>
2025-10-06 18:36:49 +0330

    Update user Events

--- src/events/user.events.js
+++ src/events/user.events.js
@@ -4,50 +4,32 @@ const path = require("path");
 const usersLogFile = path.join(__dirname, "../logs/user.logs.log");
 const userEmitter = new EventEmitter();
 
-userEmitter.on("userRegistered", (message) => {
+function logUserEvent(eventName, user) {
   const logData = {
-    message: "User registered",
-    user: message,
+    message: eventName,
+    user,
   };
+
   fs.readFile(usersLogFile, "utf8", (err, data) => {
     if (err) {
       console.error("Corrupted log file, starting fresh.");
     }
     let logs = data ? JSON.parse(data) : [];
-
     logs.push(logData);
 
     fs.writeFile(usersLogFile, JSON.stringify(logs, null, 2), (err) => {
       if (err) {
         console.error("Failed to update log:", err);
       } else {
-        console.log("[LOG] User registered successfully logged.");
+        console.log(`[LOG] ${eventName} successfully logged.`);
       }
     });
   });
-});
+}
 
-userEmitter.on("userLoggedIn", (message) => {
-  const logData = {
-    message: "User logged in",
-    user: message,
-  };
-  fs.readFile(usersLogFile, "utf8", (err, data) => {
-    if (err) {
-      console.error("Corrupted log file, starting fresh.");
-    }
-    let logs = data ? JSON.parse(data) : [];
-
-    logs.push(logData);
-
-    fs.writeFile(usersLogFile, JSON.stringify(logs, null, 2), (err) => {
-      if (err) {
-        console.error("Failed to update log:", err);
-      } else {
-        console.log("[LOG] User logged in successfully logged.");
-      }
-    });
-  });
-});
+userEmitter.on("userRegistered", (user) =>
+  logUserEvent("User registered", user)
+);
+userEmitter.on("userLoggedIn", (user) => logUserEvent("User logged in", user));
 
 module.exports = userEmitter;
