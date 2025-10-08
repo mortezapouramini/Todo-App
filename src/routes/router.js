cc11c73c584f558c4d86ebae5cdfe6077f44f635
mortezapouramini <mortezapouramini2323@example.com>
2025-10-08 10:58:23 +0330

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,11 @@
 const userRoutes = require("./user.routes");
+const taskRoutes = require("./task.routes");
 
 const router = (req, res) => {
   if (req.url.startsWith("/auth")) {
     userRoutes(req, res);
   } else if (req.url.startsWith("/tasks")) {
-    // tasks routes
+    taskRoutes(req , res)
   } else {
     res.writeHead(404, { "Content-type": "text/plain" });
     return res.end("Not found");
